4ba069249d17bb3cfb8e91fc3a337857af606a88
Evan Duffield <duffieldevan@gmail.com>
2023-09-15 22:02:19 -0600

    Testthis

--- Student/04/Gallery/photos/views.py
+++ Student/04/Gallery/photos/views.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 from django.views.generic import TemplateView
 
+heroes = {
+    # Name, Strengths, Weaknesses
+    1 : ["Richard Nixon", "Cool new advancements in electronics, swagger", "Canada, The TRUTH"],
+    2 : ["Peter Griffin", "Middle class income, strong support system", "Alcoholism, His Wife"],
+    3 : ["Howard Hughes", "Absolutely decimating a steak, staying in a movie theatre for 4 months", "OCD, Planes"]
+}
 
 def photo_list():
     def photo_details(i, f):
@@ -11,15 +17,15 @@ def photo_list():
     return photos
 
 
-class PhotoListView(TemplateView):
-    template_name = 'photos.html'
+class HeroListView(TemplateView):
+    template_name = 'heroes.html'
 
     def get_context_data(self, **kwargs):
         return dict(photos=photo_list())
 
 
-class PhotoDetailView(TemplateView):
-    template_name = 'photo.html'
+class HeroDetailView(TemplateView):
+    template_name = 'hero.html'
 
     def get_context_data(self, **kwargs):
         i = kwargs['id']
